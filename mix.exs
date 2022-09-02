bf5d585f28b67881b7397134e631815d61a8acb4
Shou Ya <shouya@users.noreply.github.com>
2022-09-02 14:14:58 +0800

    prepare for publish

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule Tx.MixProject do
   def project do
     [
       app: :tx,
+      description: "A composable database transaction library",
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,7 +23,17 @@ defmodule Tx.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ecto, ">= 0.0.0"}
+      {:ecto, ">= 0.0.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      name: "tx",
+      files: ~w(lib mix.exs README.md LICENSE),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/shouya/ckini"}
     ]
   end
 end
