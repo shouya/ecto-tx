699a72d34e60dd8367c0d38651bf3e1c419e1d10
Shou Ya <shouya@users.noreply.github.com>
2023-03-02 14:35:56 +0800

    bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tx.MixProject do
     [
       app: :tx,
       description: "A composable database transaction library",
-      version: "0.3.0",
+      version: "0.4.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       package: package(),
